e70cceb7db4e41a76d0667ab638abd44ac71f1c9
SergSi <91366575+SergSi@users.noreply.github.com>
2023-12-28 21:05:55 +0300

    Update

--- assets/js/opacity.js
+++ assets/js/opacity.js
@@ -1,71 +1,272 @@
-/*
-wwww.tigir.com - 06.07.2006
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+   
+    switch(m) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6: 
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+        text_m = "начало";
+        break;
+    case 11:
+    case 12:
+    case 13:   
+    case 14:
+        text_m = "почти четверть";
+        break;
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:   
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+    case 24:
+    case 25:  
+        text_m = "четверть";
+        break;
+    case 26:
+    case 27:
+    case 28:
+    case 29:
+        text_m = "почти половина";
+        break;
+    case 30:
+    case 31:   
+    case 32:
+    case 33:
+    case 34:
+    case 35:
+        text_m = "половина";
+        break;
+    case 36:
+    case 37:  
+    case 38:
+    case 39:
+    case 40:
+        text_m = "вторая половина";
+        break;} 
+  if (m < 41) {
+    switch(h) {
+    case 0:
+        text_h = "первого часа ночи";
+        break;    
+    case 1:
+        text_h = "второго часа ночи";
+        break;
+    case 2:
+        text_h = "третьего часа утра";
+        break;
+    case 3:
+        text_h = "четвертого часа утра";
+        break;
+    case 4:
+        text_h = "пятого часа утра";
+        break;
+    case 5:
+        text_h = "шестого часа утра";
+        break;
+    case 6:
+        text_h = "седьмого часа утра";
+        break;
+    case 7:
+        text_h = "восьмого часа утра";
+        break;
+    case 8:
+        text_h = "девятого часа утра";
+        break;
+    case 9:
+        text_h = "десятого часа дня";
+        break;
+    case 10:
+        text_h = "одиннадцатого часа дня";
+        break;
+    case 11:
+        text_h = "двенадцатого часа дня";
+        break;
+    case 12:
+        text_h = "первого часа дня";
+        break;
+    case 13:
+        text_h = "второго часа дня";
+        break;
+    case 14:
+        text_h = "третьего часа дня";
+        break;
+    case 15:
+        text_h = "четвертого часа дня";
+        break;
+    case 16:
+        text_h = "пятого часа дня";
+        break;
+    case 17:
+        text_h = "шестого часа";
+        break;
+    case 18:
+        text_h = "седьмого часа";
+        break;
+    case 19:
+        text_h = "восьмого часа вечера";
+        break;
+    case 20:
+        text_h = "девятого часа вечера";
+        break;
+    case 21:
+        text_h = "десятого часа вечера";
+        break;
+    case 22:
+        text_h = "одиннадцатого часа вечера";
+        break;
+    case 23:
+        text_h = "двенадцатого часа ночи";
+        break;
+    default:  
+        text_h = "неизвестного часа";
+        break;
+    } 
+  }    
 
-Source: http://www.tigir.com/js/opacity.js
+    switch(m) {
+    case 41:
+    case 42:
+    case 43:    
+    case 44:
+        text_m = "почти без четверти";
+        break;
+    case 45:
+    case 46:
+    case 47:
+    case 48:
+    case 49:   
+    case 50:
+        text_m = "без четверти";
+        break;    
+    case 51:
 
-Библиотека opacity.js к статье "CSS прозрачность (css opacity, javascript opacity)" - http://www.tigir.com/opacity.htm
+    case 52:
+    case 53:
+    case 54:
+    case 55:
+        text_m = "скоро";
+        break;
+    case 56:
+    case 57:
+    case 58:
+    case 59:
+        text_m = "без малого";
+        break; 
+    }
+  if (m > 40) {
+    switch(h) {
+    case 0:
+        text_h = "час ночи";
+        break;    
+    case 1:
+        text_h = "два часа ночи";
+        break;
+    case 2:
+        text_h = "три часа ночи";
+        break;
+    case 3:
+        text_h = "четыре часа утра";
+        break;
+    case 4:
+        text_h = "пять часова утра";
+        break;
+    case 5:
+        text_h = "шесть часоа утра";
+        break;
+    case 6:
+        text_h = "семь часа утра";
+        break;
+    case 7:
+        text_h = "восемь часов утра";
+        break;
+    case 8:
+        text_h = "девять часов утра";
+        break;
+    case 9:
+        text_h = "десять часов дня";
+        break;
+    case 10:
+        text_h = "одиннадцать часов дня";
+        break;
+    case 11:
+        text_h = "двенадцать часов дня";
+        break;
+    case 12:
+        text_h = "час дня";
+        break;
+    case 13:
+        text_h = "два часа дня";
+        break;
+    case 14:
+        text_h = "три часа дня";
+        break;
+    case 15:
+        text_h = "четыре часа дня";
+        break;
+    case 16:
+        text_h = "пять часов";
+        break;
+    case 17:
+        text_h = "шесть часов";
+        break;
+    case 18:
+        text_h = "семь часов";
+        break;
+    case 19:
+        text_h = "восемь часов вечера";
+        break;
+    case 20:
+        text_h = "девять часов вечера";
+        break;
+    case 21:
+        text_h = "десять часов вечера";
+        break;
+    case 22:
+        text_h = "одиннадцать часов вечера";
+        break;
+    case 23:
+        text_h = "полночь";
+        break;
+    default:  
+        text_h = "неизвестного часа";
+    }   
+  }    
+        
+    var time = text_m + " " + text_h;    
+    var delta = document.getElementById("MyClockDisplay").textContent; 
 
-setElementOpacity - установка прозрачности
-getOpacityProperty - проверка, есть ли возможность менять прозрачность
-fadeOpacity - плавное изменение прозрачности
-*/
-
-/* Функция кроссбраузерной установки прозрачности
-
-Пример: setElementOpacity(document.body, 0.5); //сделать документ прозрачным на половину
-*/
-function setElementOpacity(oElem, nOpacity)
-{
-	var p = getOpacityProperty();
-	(setElementOpacity = p=="filter"?new Function('oElem', 'nOpacity', 'nOpacity *= 100;	var oAlpha = oElem.filters["DXImageTransform.Microsoft.alpha"] || oElem.filters.alpha;	if (oAlpha) oAlpha.opacity = nOpacity; else oElem.style.filter += "progid:DXImageTransform.Microsoft.Alpha(opacity="+nOpacity+")";'):p?new Function('oElem', 'nOpacity', 'oElem.style.'+p+' = nOpacity;'):new Function)(oElem, nOpacity);
+    if (delta != time) {
+        let element = document.getElementById('MyClockDisplay');
+        let opacity = 0;
+        element.style.opacity = opacity;
+        document.getElementById("MyClockDisplay").innerText = time;
+        document.getElementById("MyClockDisplay").textContent = time;
+        let interval = setInterval(function() {
+        if (opacity < 1) {
+        opacity += 0.05;
+        element.style.opacity = opacity;
+        } else {
+        clearInterval(interval);
+        }
+        }, 100);
 }
+      
+    setTimeout(showTime, 30000); 
 
-// Функция getOpacityProperty() возвращает свойство которое используется для смены прозрачности или undefined, и может использоваться для проверки возможности изменения прозрачности
-function getOpacityProperty()
-{
-	var p;
-	if (typeof document.body.style.opacity == 'string') p = 'opacity';
-	else if (typeof document.body.style.MozOpacity == 'string') p =  'MozOpacity';
-	else if (typeof document.body.style.KhtmlOpacity == 'string') p =  'KhtmlOpacity';
-	else if (document.body.filters && navigator.appVersion.match(/MSIE ([\d.]+);/)[1]>=5.5) p =  'filter';
-	
-	return (getOpacityProperty = new Function("return '"+p+"';"))();
 }
-
-/* Функции для плавного изменения прозрачности:
-
-1) fadeOpacity.addRule('opacityRule1', 1, 0.5, 30); //вначале создаем правило, задаем имя правила, начальную прозрачность и конечную, необязательный параметр задержки, влийяющий на скорость смены прозрачности
-2) fadeOpacity('elemID', 'opacityRule1'); // выполнить плавную смену прозрачности элемента с id равным elemID, по правилу opacityRule1
-3) fadeOpacity.back('elemID'); //вернуться в исходное сотояние прозрачности
-*/
-function fadeOpacity(sElemId, sRuleName, bBackward)
-{
-	var elem = document.getElementById(sElemId);
-	if (!elem || !getOpacityProperty() || !fadeOpacity.aRules[sRuleName]) return;
-	
-	var rule = fadeOpacity.aRules[sRuleName];
-	var nOpacity = rule.nStartOpacity;
-	
-	if (fadeOpacity.aProc[sElemId]) {clearInterval(fadeOpacity.aProc[sElemId].tId); nOpacity = fadeOpacity.aProc[sElemId].nOpacity;}
-	if ((nOpacity==rule.nStartOpacity && bBackward) || (nOpacity==rule.nFinishOpacity && !bBackward)) return;
-
-	fadeOpacity.aProc[sElemId] = {'nOpacity':nOpacity, 'tId':setInterval('fadeOpacity.run("'+sElemId+'")', fadeOpacity.aRules[sRuleName].nDalay), 'sRuleName':sRuleName, 'bBackward':Boolean(bBackward)};
-}
-
-fadeOpacity.addRule = function(sRuleName, nStartOpacity, nFinishOpacity, nDalay){fadeOpacity.aRules[sRuleName]={'nStartOpacity':nStartOpacity, 'nFinishOpacity':nFinishOpacity, 'nDalay':(nDalay || 30),'nDSign':(nFinishOpacity-nStartOpacity > 0?1:-1)};};
-
-fadeOpacity.back = function(sElemId){fadeOpacity(sElemId,fadeOpacity.aProc[sElemId].sRuleName,true);};
-
-fadeOpacity.run = function(sElemId)
-{
-	var proc = fadeOpacity.aProc[sElemId];
-	var rule = fadeOpacity.aRules[proc.sRuleName];
-	
-	proc.nOpacity = Math.round(( proc.nOpacity + .1*rule.nDSign*(proc.bBackward?-1:1) )*10)/10;
-	setElementOpacity(document.getElementById(sElemId), proc.nOpacity);
-	
-	if (proc.nOpacity==rule.nStartOpacity || proc.nOpacity==rule.nFinishOpacity) clearInterval(fadeOpacity.aProc[sElemId].tId);
-}
-fadeOpacity.aProc = {};
-fadeOpacity.aRules = {};
-
+  
+showTime();
