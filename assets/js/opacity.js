ae24a72ca409646ef1f9cfa69cfff6b9f8d77460
SergSi <91366575+SergSi@users.noreply.github.com>
2023-12-30 09:42:43 +0300

    Update

--- assets/js/opacity.js
+++ assets/js/opacity.js
@@ -339,7 +339,7 @@ function showTime(){
   }    
           
           
-    var time = "—" + " " + text_m + " " + text_h + " " + "—";    
+    var time = text_m + " " + text_h;    
     var delta = document.getElementById("MyClockDisplay").textContent; 
 
     if ((delta != time)) {
