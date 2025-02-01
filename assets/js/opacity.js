36281a51efd3217515ed9ac21146b7e1d699a4af
SergSi <91366575+SergSi@users.noreply.github.com>
2025-02-01 21:51:18 +0300

    Update opacity.js

--- assets/js/opacity.js
+++ assets/js/opacity.js
@@ -307,7 +307,7 @@
         }
 
         // Обновляем часы каждую секунду
-        setInterval(updateClock, 1000);
+        setInterval(updateClock, 60000);
         // Инициализация при загрузке страницы
         updateClock();
 
